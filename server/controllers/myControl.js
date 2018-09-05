a9c98ca1e4f0faeba60ef8784c5198fc668b6a0c
jobbos <josephjobbos@gmail.com>
2018-09-05 02:13:48 -0700

    (put orders):update order details api endpoint
    created controller for updating food order details
    created route
    [delivered #160278608]
    "

--- server/controllers/myControl.js
+++ server/controllers/myControl.js
@@ -1,9 +1,10 @@
 import myFoods from './../models/list'
+import { request } from 'https';
 
 
 export default class MyController {
     static getAllFoods(req,res) {
-        res.status(200).send({
+       return res.status(200).send({
             status:'success',
             message:'successfully retrived all school',
             myFoods
@@ -23,7 +24,7 @@ export default class MyController {
        }
                static createNewOrder(req,res){
                    const newId = myFoods[myFoods.length - 1].foodId + 1;
-                   for(var i =0; i < myFoods.length; i++) {
+                   for(let i =0; i < myFoods.length; i++) {
                        if(myFoods[i].name === req.body.name) {
                            res.send({
                             message:'there is a school with the name already, name must be unique'
@@ -43,13 +44,28 @@ export default class MyController {
                     message:'successfully created',
                     myFoods
                 })
-
-
- 
                }
 
-
-
+               static modifyInput(req, res) {
+                const {name, amount, quantity, deliveryAddress} = req.body;
+                for(var i = 0; i < myFoods.length; i++) {
+                    if(myFoods[i].foodId === parseInt(req.params.foodID)) {
+                        myFoods[i].name = name || myFoods[i].name
+                        myFoods[i].amount = amount || myFoods[i].amount
+                        myFoods[i].quantity = amount || myFoods[i].quantity
+                        myFoods[i].deliveryAddress = deliveryAddress || myFoods[i].deliveryAddress
+                        res.send(myFoods)
+                    }else {
+                        if(myFoods[i].name === name) {
+                           return res.send({message:'food name already exist'})
+                        }
+                    }
+                } return res.send({      
+                    status: 'failed',
+                    message: `food with id ${parseInt(req.params.foodID)} does not exit`
+                })
+               }
+              
 
     }
 
