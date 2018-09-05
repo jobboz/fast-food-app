3a0f670c397b00b0a97eaeae26a1060775630624
jobbos <josephjobbos@gmail.com>
2018-09-05 00:00:49 -0700

    feat(post new order):post new order api endpoint
    created controller for posting order details
    created middleware for validating input
    created route
    [delivered #160250349]

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,13 +1,15 @@
 import express from "express";
 import MyController from './../controllers/myControl';
 const router = express.Router();
-
+import foodValidate from './../middleware/validate';
 
 router.route('/foods')
-.get(MyController.getAllFoods)
+    .get(MyController.getAllFoods)
+    .post(foodValidate.createFoodValidator, MyController.createNewOrder)
 
 router.route('/foods/:foodID')
-.get(MyController.getSpecificFoods)
+    .get(MyController.getSpecificFoods)
+
 
 
 
