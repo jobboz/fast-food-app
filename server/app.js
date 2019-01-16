2deeb64ee6484fd3babf4c1329211d4c2fb3fce2
github_jobboz <josephjobbos@gmail.com>
2019-01-16 13:46:45 -0400

    dddd

--- server/app.js
+++ server/app.js
@@ -2,12 +2,11 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import rout from './routes/route';
 const app = express();
-var cors = require('cors')
+import cors from 'cors';
  
-app.use(cors())
 const port = process.env.PORT || 5000;
 
-
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.get('/',  (req, res) => {
